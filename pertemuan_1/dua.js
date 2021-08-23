53139aa7a254fa9721fbe2d6b0ab8513dc7e3ac6
Edbert <robinsonedbertxvi@gmail.com>
2021-08-23 16:03:58 +0800

    initial commit

---
+++ pertemuan_1/dua.js
@@ -0,0 +1,5 @@
+let months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "Octomber", "November", "December" ]
+
+for(let i = 0; i < 12; i++) {
+	console.log(i+1, "=", months[i])
+}
---
+++ pertemuan_1/tiga.js
@@ -0,0 +1,4 @@
+var name = "Saya akan menjadi Backend Enginerer"
+for(let i =0; i<22; i = i+2){
+	console.log(i , "-", name)
+}
